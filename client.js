79eb5ba76016a3a6874b433edf91829e0540c018
Vitalii Stepaniuk <vitalii.stepaniuk@gmail.com>
2019-09-14 13:05:22 +0300

    Client adjustments to recieve mqtt message and send it to websocket.

--- client.js
+++ client.js
@@ -1,6 +1,16 @@
 const WebSocket = require('ws')
 const url = 'ws://localhost:8082'
 const connection = new WebSocket(url)
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://localhost', {username: 'mosquitto_almost_fly', password: 'thatistrue'})
+
+client.on('connect', function () {
+    client.subscribe('hello/world', function (err) {
+        if (!err) {
+            client.publish('woteva', 'This is yet another woteva')
+        }
+    })
+})
 
 connection.onopen = () => {
     connection.send('Message for client')
@@ -11,3 +21,8 @@ connection.onerror = (error) => {
 connection.onmessage = (e) => {
     console.log(e.data)
 }
+
+client.on('message', function(topic, message) {
+    connection.send(message.toString())
+    console.log(message.toString())
+})
